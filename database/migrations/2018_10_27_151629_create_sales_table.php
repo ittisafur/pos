4828b73d90fe841a9d2b89369c9602c807e8a76d
undefined <ittisafur>
2018-10-28 22:57:33 +0600

    First work

---
+++ database/migrations/2018_10_27_151629_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('product_id');
+            $table->integer('discount')->nullable();
+            $table->integer('revenue')->nullable();
+            $table->integer('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
